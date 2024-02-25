54adfed2d533b54374bdbe5e6a8eab8fb55ea576
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-25 16:53:26 +0300

    Ms.Services.CouponAPI created

---
+++ Services/Ms.Services.CouponAPI/Program.cs
@@ -0,0 +1,51 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Ms.Services.CouponAPI;
+using Ms.Services.CouponAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+//sql server connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//automapper
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+ApplyMigration();
+
+app.Run();
+
+void ApplyMigration()
+{
+    using var serviceScope = app.Services.CreateScope();
+    var context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
