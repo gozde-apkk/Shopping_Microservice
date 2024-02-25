c27dbc2a176fb4d9fbef2dc54fa8ab8e2e805f54
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-25 16:49:45 +0300

    added migration file

---
+++ Services/Ms.Services.CouponAPI/Migrations/20240225095025_SeedCouponTables.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ms.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCouponTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CouponCode",
+                table: "Coupons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "10OFF", 10.0, 20 },
+                    { 2, "20OFF", 20.0, 40 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "CouponCode",
+                table: "Coupons");
+        }
+    }
+}
