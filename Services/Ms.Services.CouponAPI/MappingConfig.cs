d45dac6e96576a2c05c37798ede10cd7fe445450
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-25 16:52:36 +0300

    createdin mapping configuration

---
+++ Services/Ms.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ms.Services.CouponAPI.Models;
+using Ms.Services.CouponAPI.Models.Dto;
+
+namespace Ms.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
