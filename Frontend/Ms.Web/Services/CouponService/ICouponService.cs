74c1b9ea58f56df5f1b45dcb409a6e9fd9229e95
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-25 17:19:18 +0300

    coupon services in web project

---
+++ Frontend/Ms.Web/Services/CouponService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using Ms.Web.Models;
+
+namespace Ms.Web.Services.CouponService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponsAsync(int id);
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto);
+    }
+}
