9162adf63333feec8dc42ec7b1854b86d8903f23
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 11:55:46 +0300

    created IJwtTokenGenerator and  JwtTokenGenerator service

---
+++ Services/Ms.Services.AuthAPI/Services/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Ms.Services.AuthAPI.Models;
+
+namespace Ms.Services.AuthAPI.Services
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
