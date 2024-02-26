8bba4c2efc81c01287f23a1403c9b1ffbbecbbf0
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 10:31:27 +0300

    created login response dto

---
+++ Services/Ms.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ms.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
