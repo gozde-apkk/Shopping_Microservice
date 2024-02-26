6c3bc16ef7aefb9536df47be49d2fec55ab6fb8c
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 10:32:09 +0300

    created register request dto

---
+++ Services/Ms.Services.AuthAPI/Models/Dto/RegisterRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ms.Services.AuthAPI.Models.Dto
+{
+    public class RegisterRequestDto
+    {
+
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
