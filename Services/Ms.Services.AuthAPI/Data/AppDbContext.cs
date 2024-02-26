12c90824811f9855225e4279b22ae8129847c651
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 09:55:59 +0300

    created app db context

---
+++ Services/Ms.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Ms.Services.AuthAPI.Models;
+
+
+namespace Ms.Services.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
+
+
+           
