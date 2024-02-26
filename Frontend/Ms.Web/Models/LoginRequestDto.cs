64a4ed79846465840d1d7abffee1264709bfd577
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 12:33:40 +0300

    created dto in web models

---
+++ Frontend/Ms.Web/Models/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ms.Web.Model
+{
+    public class LoginRequestDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Frontend/Ms.Web/Models/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ms.Web.Model
+{
+    public class LoginResponseDto
+    {
+
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
