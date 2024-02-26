d1e66d49157ac937db3cb1b58e3b9170b5cc9ff2
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 12:35:38 +0300

    created auth service

---
+++ Frontend/Ms.Web/Services/AuthService/AuthService.cs
@@ -0,0 +1,45 @@
+﻿using Ms.Web.Model;
+using Ms.Web.Models;
+using Ms.Web.Utility;
+
+namespace Ms.Web.Services.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> AssignRoleAsync(RegisterRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/AssignRole"
+            });
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = loginRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/login"
+            });
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegisterRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/register"
+            });
+        }
+    }
+}
