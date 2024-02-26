10bac82454e5f0dfd91fe702111f77823dbdccd7
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 11:54:47 +0300

    created IAuthService and AuthService

---
+++ Services/Ms.Services.AuthAPI/Services/AuthService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Ms.Services.AuthAPI.Data;
+using Ms.Services.AuthAPI.Models;
+using Ms.Services.AuthAPI.Models.Dto;
+
+namespace Ms.Services.AuthAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IJwtTokenGenerator jwtTokenGenerator   )
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public async Task<string> Register(RegisterRequestDto registerRequestDto)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = registerRequestDto.Email,
+                Email = registerRequestDto.Email,
+                NormalizedEmail = registerRequestDto.Email.ToUpper(),
+                Name = registerRequestDto.Name,
+                PhoneNumber = registerRequestDto.PhoneNumber
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registerRequestDto.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u => u.UserName == registerRequestDto.Name);
+
+                    UserDto userDto = new()
+                    {
+                        Email = userToReturn.Email,
+                        Name = userToReturn.Name,
+                        PhoneNumber = userToReturn.PhoneNumber,
+                        ID = userToReturn.Id
+                    };
+                    return "";
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return "Error Encountered while registering user";
+        }
+
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+          var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+            if(user == null || isValid == false)
+            {
+                return new LoginResponseDto() { User = null, Token = "" };
+            }
+
+            //if user was found, Generate JWT Token
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            UserDto userDto = new()
+            {
+                ID = user.Id,
+                Email = user.Email,
+                Name = user.Name,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                User = userDto,
+                Token = token
+            };
+
+            return loginResponseDto;
+        }
+    }
+}
---
+++ Services/Ms.Services.AuthAPI/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Ms.Services.AuthAPI.Models.Dto;
+
+namespace Ms.Services.AuthAPI.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<string> Register(RegisterRequestDto registerRequestDto);
+        
+    }
+}
