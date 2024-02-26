0af0b7a3b5df76996d4a28b2b198a053b605243c
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 11:56:23 +0300

    created auth controller

---
+++ Services/Ms.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ms.Services.AuthAPI.Models.Dto;
+using Ms.Services.AuthAPI.Services;
+
+namespace Ms.Services.AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+
+        private readonly IAuthService _authService;
+        protected ResponseDto _response;
+
+
+        public AuthAPIController(IAuthService authService)
+        {
+            _authService = authService;
+            _response = new ResponseDto();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto model)
+        {
+            var errorMessage = await _authService.Register(model);
+            if(!string.IsNullOrEmpty(errorMessage))
+            {
+                _response.IsSuccess = false;
+                _response.Message = errorMessage;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+
+        {
+            var loginResponse = await _authService.Login(model);
+            if(loginResponse.User == null)
+            {
+                _response.IsSuccess = false;
+              _response.Message = "Username or password is incorrect";
+                return BadRequest(_response);
+            }
+
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+    }
+}
