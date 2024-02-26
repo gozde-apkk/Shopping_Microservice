fd3a9b93a28706fdd859975b13e39f71f66df5bc
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-26 12:36:29 +0300

    created interface auth service

---
+++ Frontend/Ms.Web/Services/AuthService/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Ms.Web.Model;
+using Ms.Web.Models;
+
+namespace Ms.Web.Services.AuthService
+{
+    public interface IAuthService
+    {
+
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> RegisterAsync(RegisterRequestDto registrationRequestDto);
+        Task<ResponseDto?> AssignRoleAsync(RegisterRequestDto registrationRequestDto);
+    }
+}
