5e0ea3a10202353eeb7bb0e786e9c2284009c5a5
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-27 12:29:21 +0300

    auth service updated

--- Services/Ms.Services.AuthAPI/Services/AuthService.cs
+++ Services/Ms.Services.AuthAPI/Services/AuthService.cs
@@ -72,8 +72,8 @@ namespace Ms.Services.AuthAPI.Services
             }
 
             //if user was found, Generate JWT Token
-
-            var token = _jwtTokenGenerator.GenerateToken(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var token = _jwtTokenGenerator.GenerateToken(user,roles);
 
             UserDto userDto = new()
             {
