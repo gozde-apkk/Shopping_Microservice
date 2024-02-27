9a9b7c963faab9012a60ec18618a649975729163
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-27 14:30:55 +0300

    mapping config in product api

---
+++ Services/Ms.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ms.Services.ProductAPI.Models;
+using Ms.Services.ProductAPI.Models.Dto;
+
+namespace Ms.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+           
+            });
+            return mappingConfig;
+        }
+    }
+}
